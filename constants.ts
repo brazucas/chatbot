c3dc7c2112515088c0ac07afeaa8002e4953c379
Pedro Papadópolis <pplp93@gmail.com>
2022-09-25 21:55:41 +1000

    First version A big messy code that somehow works

---
+++ constants.ts
@@ -0,0 +1,6 @@
+export const myMention = "@61467542295";
+export enum Groups {
+  BRZAdmin = "558699856170-1389759544@g.us",
+}
+export const audioBasePath =
+  "/Users/pedropapadopolis/Library/Mobile Documents/com~apple~CloudDocs/Documents/GTA/BRZ/Audios BRZ/";
