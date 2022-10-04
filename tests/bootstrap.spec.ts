f37e3bd7dd80abbd1701617d7c46b904684b5376
Pedro Papadópolis <pplp93@gmail.com>
2022-10-05 00:08:00 +1100

    Testing bootstrap

---
+++ tests/bootstrap.spec.ts
@@ -0,0 +1,20 @@
+import { bootstrap } from "@/bootstrap";
+import { MockClient } from "./mock.client";
+
+describe("main", () => {
+  const clients = [new MockClient([])];
+  let spyClientInitialize: jest.SpyInstance;
+
+  beforeAll(async () => {
+    spyClientInitialize = jest.spyOn(clients[0], "initialize");
+  });
+
+  afterAll(() => {
+    spyClientInitialize.mockReset();
+  });
+
+  it("should initialize clients", async () => {
+    await bootstrap(clients);
+    expect(spyClientInitialize).toBeCalled();
+  });
+});
