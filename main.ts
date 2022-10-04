c2ede11fb18c732ec56ab3ff81d868b8a6e17d31
Pedro Papadópolis <pplp93@gmail.com>
2022-10-05 00:06:53 +1100

    Spliting main into bootstrap

--- main.ts
+++ main.ts
@@ -1,19 +1,8 @@
+import { bootstrap } from "@/bootstrap";
 import { SoundsInteraction } from "@/interactions/sounds.interaction";
-import { ChatbotClient } from "@/typings";
 import { WhatsappClient } from "@whatsapp/whatsapp.client";
 
 const interactions = [new SoundsInteraction()];
+const clients = [new WhatsappClient(interactions)];
 
-(async () => {
-  console.log("Initializing...");
-
-  const providers: ChatbotClient<any>[] = [new WhatsappClient(interactions)];
-
-  for (const provider of providers) {
-    await provider.initialize();
-  }
-
-  console.log(
-    `Loaded ${providers.length} providers and ${interactions.length} interactions`
-  );
-})();
+bootstrap(clients);
---
+++ src/bootstrap.ts
@@ -0,0 +1,11 @@
+import { ChatbotClient } from "./typings";
+
+export const bootstrap = async (clients: ChatbotClient<any>[]) => {
+  console.log("Initializing...");
+
+  for (const client of clients) {
+    await client.initialize();
+  }
+
+  return console.log(`Loaded ${clients.length} providers`);
+};
