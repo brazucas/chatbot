a450c64b8124dbf912bf4c4ccd9bdff79ae9f1c9
Pedro Papadópolis <pplp93@gmail.com>
2022-10-11 23:17:05 +1100

    Better session handler file name

--- src/interactions/chat/session-handler.ts
+++ src/interactions/chat/session-handler.ts
@@ -1,29 +1,27 @@
-import { QuestionInterface } from './interface/question';
-import { QuestionCustomerAnswerInterface } from './interface/question-customer-answer';
-import { SessionInterface } from './interface/session';
-import { SessionQuestionLogInterface } from './interface/session-quesiton-log';
-import { SessionStatus } from './interface/session-status';
+import { QuestionInterface } from "./interface/question.interface";
+import { QuestionCustomerAnswerInterface } from "./interface/question-customer-answer.interface";
+import { SessionInterface } from "./interface/session.interface";
+import { SessionQuestionLogInterface } from "./interface/session-quesiton-log.interface";
+import { SessionStatus } from "./interface/session-status.enum";
 
-export class Session implements SessionInterface {
+export class SessionHandler implements SessionInterface {
   private _id: number;
-
   private _lastInteraction: Date;
-
   private _status: SessionStatus;
-
   private _currentQuestion: QuestionInterface;
-
   private _isBusy: boolean;
+  private _chatId: string;
 
   constructor({
     id,
-    lastInteraction,
+    chatId,
     status,
     currentQuestion,
     isBusy,
   }: SessionInterface) {
     this._id = id;
-    this._lastInteraction = lastInteraction;
+    this._chatId = chatId;
+    this._lastInteraction = new Date();
     this._status = status;
     this._currentQuestion = currentQuestion;
     this._isBusy = isBusy;
@@ -31,6 +29,10 @@ export class Session implements SessionInterface {
 
   get id() {
     return this._id;
+  }
+
+  get chatId() {
+    return this._chatId;
   }
 
   get lastInteraction() {
@@ -50,34 +52,34 @@ export class Session implements SessionInterface {
   }
 
   setLastInteraction(timestamp: Date): Promise<void> {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 
   storeAnswer(answer: string): Promise<QuestionCustomerAnswerInterface> {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 
   setStatus(status: SessionStatus): Promise<void> {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 
   triggerQuestionActions(): Promise<SessionQuestionLogInterface[]> {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 
   nextQuestion(): Promise<void> {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 
   previousQuestion(): Promise<void> {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 
   skipToQuestion(question: QuestionInterface): Promise<void> {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 
   setIsBusy(isBusy: boolean): Promise<void> {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 }
