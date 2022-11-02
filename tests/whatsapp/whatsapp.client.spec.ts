62b19c4e3818fcbf48d48bc024a956e94715ff62
Pedro Papadópolis <pplp93@gmail.com>
2022-11-02 22:05:51 +1100

    Add more WhatsappClient tests

--- tests/whatsapp/whatsapp.client.spec.ts
+++ tests/whatsapp/whatsapp.client.spec.ts
@@ -1,11 +1,20 @@
 import { WhatsappClient } from '@whatsapp/whatsapp.client';
-import WAWebJS from 'whatsapp-web.js';
+import WAWebJS, { Client } from 'whatsapp-web.js';
 import { InteractionResponseType } from '@src/typings';
+import * as qrcodeTerminal from 'qrcode-terminal';
 import { mockInitialize, MockInteraction, TestableApp } from '../helpers';
 
+const callbacks: {
+  [key: string]: (message: WAWebJS.Message) => void;
+} = {};
 jest.mock('whatsapp-web.js', () => ({
   Client: jest.fn(() => ({
-    on: jest.fn(),
+    on: jest.fn((event, callback) => {
+      callbacks[event] = callback;
+    }),
+    triggerCallback: (event: string, message: WAWebJS.Message) => {
+      callbacks[event](message);
+    },
     initialize: jest.fn(),
   })),
   LocalAuth: jest.fn(),
@@ -27,6 +36,15 @@ jest.mock('whatsapp-web.js', () => ({
 
 jest.mock('qrcode-terminal', () => ({
   generate: jest.fn(),
+}));
+
+const loggerWarn = jest.fn();
+jest.mock('@src/interactions/chat/service/logger.service', () => ({
+  default: {
+    logger: jest.fn(() => ({
+      warn: loggerWarn,
+    })),
+  },
 }));
 
 describe('WhatsappClient Reply', () => {
@@ -72,6 +90,17 @@ describe('WhatsappClient Reply', () => {
     );
   });
 
+  it('should throw error if evaluate response with unknown type', async () => {
+    spyClientEvaluateResponse.mockRestore();
+
+    const call = client.evaluateResponse(
+      { body: 'test', responseType: 'unknown' as any },
+      null as unknown as WAWebJS.Message,
+    );
+
+    expect(call).rejects.toThrowError();
+  });
+
   it('should fail evaluate response with no body', async () => {
     spyClientEvaluateResponse.mockRestore();
 
@@ -109,7 +138,7 @@ describe('WhatsappClient Reply', () => {
 
 describe('WhatsappClient reply privately', () => {
   let chatbot: TestableApp;
-  
+
   const client = new WhatsappClient([
     new MockInteraction(InteractionResponseType.ReplyPrivately),
   ]);
@@ -193,5 +222,289 @@ describe('WhatsappClient send audio media', () => {
         fromUrl: expect.anything(),
       }),
     );
+  });
+});
+
+describe('Whatsapp client callbacks', () => {
+  const client = new WhatsappClient([
+    new MockInteraction(InteractionResponseType.MediaAudio),
+  ]);
+
+  beforeEach(() => {
+    loggerWarn.mockReset();
+  });
+
+  const WAWebJSClient = new Client({});
+
+  it('should call qr', async () => {
+    const qr = jest.fn();
+    const qrcodeFn = qrcodeTerminal.generate;
+
+    await client.initialize();
+    (WAWebJSClient as any).triggerCallback('qr', {
+      generate: qr,
+    });
+
+    expect(loggerWarn).toBeCalledTimes(1);
+    expect(qrcodeFn).toBeCalledTimes(1);
+    expect(qrcodeFn).toBeCalledWith(expect.anything(), { small: true });
+  });
+
+  it('should call ready', async () => {
+    await client.initialize();
+    (WAWebJSClient as any).triggerCallback('ready');
+
+    expect(loggerWarn).toBeCalledWith('Client is ready!');
+  });
+
+  it('should call disconnected', async () => {
+    await client.initialize();
+    (WAWebJSClient as any).triggerCallback('disconnected');
+
+    expect(loggerWarn).toBeCalledWith('Client disconnected!');
+  });
+
+  it('should call auth_failure', async () => {
+    await client.initialize();
+    (WAWebJSClient as any).triggerCallback('auth_failure', 'test');
+
+    expect(loggerWarn).toBeCalledTimes(2);
+    expect(loggerWarn).toHaveBeenCalledWith('Auth failure', 'test');
+  });
+
+  it('should call authenticated', async () => {
+    await client.initialize();
+    (WAWebJSClient as any).triggerCallback('authenticated');
+
+    expect(loggerWarn).toBeCalledWith('authenticated');
+  });
+
+  it('should call client digest on message_create webhook', async () => {
+    const spyDigestmessage = jest.spyOn(client as any, 'digestMessage');
+
+    await client.initialize();
+
+    const msg = {
+      fromMe: true,
+      from: 'test',
+      id: {
+        _serialized: 'id',
+      },
+      body: 'body',
+      author: {},
+      authorName: 'authorName',
+      chatId: 'chatId',
+      client: {},
+      getContact: jest.fn(async () => ({
+        name: 'test',
+      })),
+    };
+    await (WAWebJSClient as any).triggerCallback('message_create', msg);
+
+    expect(spyDigestmessage).toHaveBeenLastCalledWith(expect.objectContaining({
+      id: expect.anything(),
+      body: expect.anything(),
+      author: expect.anything(),
+      authorName: expect.anything(),
+      chatId: expect.anything(),
+      client: expect.anything(),
+    }), expect.anything());
+
+    spyDigestmessage.mockReset();
+  });
+
+  it('shouldn\'t call client digest on message_create webhook if message not from user', async () => {
+    const spyDigestmessage = jest.spyOn(client as any, 'digestMessage');
+
+    await client.initialize();
+
+    const msg = {
+      fromMe: false,
+      from: 'test',
+      id: {
+        _serialized: 'id',
+      },
+      body: 'body',
+      author: {},
+      authorName: 'authorName',
+      chatId: 'chatId',
+      client: {},
+      getContact: jest.fn(async () => ({
+        name: 'test',
+      })),
+    };
+    await (WAWebJSClient as any).triggerCallback('message_create', msg);
+
+    expect(spyDigestmessage).not.toHaveBeenCalled();
+  });
+
+  it('should call client digest on message webhook', async () => {
+    const spyDigestmessage = jest.spyOn(client as any, 'digestMessage');
+
+    await client.initialize();
+
+    const msg = {
+      fromMe: true,
+      from: 'test',
+      id: {
+        _serialized: 'id',
+      },
+      body: 'body',
+      author: {},
+      authorName: 'authorName',
+      chatId: 'chatId',
+      client: {},
+      getContact: jest.fn(async () => ({
+        name: 'test',
+      })),
+    };
+    await (WAWebJSClient as any).triggerCallback('message', msg);
+
+    expect(spyDigestmessage).toHaveBeenLastCalledWith(expect.objectContaining({
+      id: expect.anything(),
+      body: expect.anything(),
+      author: expect.anything(),
+      authorName: expect.anything(),
+      chatId: expect.anything(),
+      client: expect.anything(),
+    }), expect.anything());
+
+    spyDigestmessage.mockReset();
+  });
+
+  it('should call client digest on message webhook with default author', async () => {
+    const spyDigestmessage = jest.spyOn(client as any, 'digestMessage');
+
+    await client.initialize();
+
+    const msg = {
+      fromMe: true,
+      from: 'test',
+      id: {
+        _serialized: 'id',
+      },
+      body: 'body',
+      author: null,
+      authorName: 'authorName',
+      chatId: 'chatId',
+      client: {},
+      getContact: jest.fn(async () => ({
+        name: 'test',
+      })),
+    };
+    await (WAWebJSClient as any).triggerCallback('message', msg);
+
+    expect(spyDigestmessage).toHaveBeenLastCalledWith(expect.objectContaining({
+      id: expect.anything(),
+      body: expect.anything(),
+      author: '',
+      authorName: expect.anything(),
+      chatId: expect.anything(),
+      client: expect.anything(),
+    }), expect.anything());
+
+    spyDigestmessage.mockReset();
+  });
+
+  it('should call client digest on message webhook with default authorName', async () => {
+    const spyDigestmessage = jest.spyOn(client as any, 'digestMessage');
+
+    await client.initialize();
+
+    const msg = {
+      fromMe: true,
+      from: 'test',
+      id: {
+        _serialized: 'id',
+      },
+      body: 'body',
+      author: {},
+      authorName: 'authorName',
+      chatId: 'chatId',
+      client: {},
+      getContact: jest.fn(async () => ({
+        name: null,
+      })),
+    };
+    await (WAWebJSClient as any).triggerCallback('message', msg);
+
+    expect(spyDigestmessage).toHaveBeenLastCalledWith(expect.objectContaining({
+      id: expect.anything(),
+      body: expect.anything(),
+      author: expect.anything(),
+      authorName: '',
+      chatId: expect.anything(),
+      client: expect.anything(),
+    }), expect.anything());
+
+    spyDigestmessage.mockReset();
+  });
+
+  it('should call client digest on message_create webhook with default author', async () => {
+    const spyDigestmessage = jest.spyOn(client as any, 'digestMessage');
+
+    await client.initialize();
+
+    const msg = {
+      fromMe: true,
+      from: 'test',
+      id: {
+        _serialized: 'id',
+      },
+      body: 'body',
+      author: null,
+      authorName: 'authorName',
+      chatId: 'chatId',
+      client: {},
+      getContact: jest.fn(async () => ({
+        name: 'test',
+      })),
+    };
+    await (WAWebJSClient as any).triggerCallback('message_create', msg);
+
+    expect(spyDigestmessage).toHaveBeenLastCalledWith(expect.objectContaining({
+      id: expect.anything(),
+      body: expect.anything(),
+      author: '',
+      authorName: expect.anything(),
+      chatId: expect.anything(),
+      client: expect.anything(),
+    }), expect.anything());
+
+    spyDigestmessage.mockReset();
+  });
+
+  it('should call client digest on message_create webhook with default authorName', async () => {
+    const spyDigestmessage = jest.spyOn(client as any, 'digestMessage');
+
+    await client.initialize();
+
+    const msg = {
+      fromMe: true,
+      from: 'test',
+      id: {
+        _serialized: 'id',
+      },
+      body: 'body',
+      author: {},
+      authorName: 'authorName',
+      chatId: 'chatId',
+      client: {},
+      getContact: jest.fn(async () => ({
+        name: null,
+      })),
+    };
+    await (WAWebJSClient as any).triggerCallback('message_create', msg);
+
+    expect(spyDigestmessage).toHaveBeenLastCalledWith(expect.objectContaining({
+      id: expect.anything(),
+      body: expect.anything(),
+      author: expect.anything(),
+      authorName: '',
+      chatId: expect.anything(),
+      client: expect.anything(),
+    }), expect.anything());
+
+    spyDigestmessage.mockReset();
   });
 });
