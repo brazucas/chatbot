2fe43f968b6a392939618aa4f1d98897fa853c5d
Pedro Papadópolis <pplp93@gmail.com>
2022-12-22 22:12:41 +1100

    Add prisma service tests

--- tests/interactions/chat/service/prisma.service.test.ts
+++ tests/interactions/chat/service/prisma.service.test.ts
@@ -1,5 +1,11 @@
 import PrismaService from '@src/interactions/chat/service/prisma.service';
 
+jest.mock('@prisma/client', () => ({
+  PrismaClient: jest.fn().mockImplementation(() => ({
+    $connect: jest.fn(),
+  })),
+}));
+
 describe('PrismaService', () => {
   it('should be a singleton', () => {
     const instance1 = PrismaService.getInstance();
