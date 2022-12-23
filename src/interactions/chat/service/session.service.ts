6211897bb5ccb19551b4dcfea3295162a4a9675e
Pedro Papadópolis <pplp93@gmail.com>
2022-12-23 22:25:44 +1100

    Add create session tests

--- src/interactions/chat/service/session.service.ts
+++ src/interactions/chat/service/session.service.ts
@@ -1,19 +1,43 @@
-import { Session } from '@prisma/client';
+import { Prisma } from '@prisma/client';
 import { SessionHandlerInterface } from '@src/interactions/chat/types/session-handler.interface';
-import { SessionServiceInterface } from '../types/session-service.interface';
 import SessionHandler from '../session-handler';
+import { SessionServiceInterface } from '../types/session-service.interface';
 import AbstractService from './abstract.service';
+import PrismaService from './prisma.service';
+import QuestionService from './question.service';
 
 export default class SessionService
   extends AbstractService
   implements SessionServiceInterface {
-  public constructor() {
+  readonly sessionRelations = Prisma.validator<Prisma.SessionArgs>()({
+    include: {
+      currentQuestion: true,
+    },
+  });
+
+  public constructor(
+    protected readonly prisma = PrismaService.getInstance(),
+    protected readonly questionSvc = QuestionService.getInstance<QuestionService>(),
+  ) {
     super();
   }
 
   // eslint-disable-next-line
   public async create(chatId: string): Promise<SessionHandlerInterface> {
-    throw new Error('Method not implemented.');
+    const firstQuestion = await this.questionSvc.findFirst();
+
+    return this.prisma.session.create({
+      ...this.sessionRelations,
+      data: {
+        chatId,
+        customer: 'customer',
+        currentQuestion: {
+          connect: {
+            id: firstQuestion.id,
+          },
+        },
+      },
+    }).then((session) => this.buildSessionHandler(session));
   }
 
   // eslint-disable-next-line
@@ -26,8 +50,34 @@ export default class SessionService
     throw new Error('Method not implemented.');
   }
 
-  // eslint-disable-next-line
-  public async buildSessionHandler(session: Session): Promise<SessionHandler> {
-    throw new Error('Method not implemented.');
+  public async buildSessionHandler({
+    customer,
+    id,
+    chatId,
+    createdAt,
+    currentQuestionId,
+    lastInteraction,
+    status,
+    currentQuestion,
+  }: Prisma.SessionGetPayload<
+  typeof this.sessionRelations
+  >): Promise<SessionHandler> {
+    return new SessionHandler({
+      currentQuestion: {
+        createdAt: currentQuestion.createdAt,
+        id: currentQuestion.id,
+        isActive: currentQuestion.isActive,
+        parentId: currentQuestion.parentId,
+        title: currentQuestion.title,
+        type: currentQuestion.type,
+      },
+      customer,
+      id,
+      chatId,
+      createdAt,
+      currentQuestionId,
+      lastInteraction,
+      status,
+    });
   }
 }
--- tests/interactions/chat/service/session.service.test.ts
+++ tests/interactions/chat/service/session.service.test.ts
@@ -1,9 +1,86 @@
+import { QuestionType } from '@prisma/client';
+import PrismaService from '@src/interactions/chat/service/prisma.service';
+import QuestionService from '@src/interactions/chat/service/question.service';
 import SessionService from '@src/interactions/chat/service/session.service';
+import SessionHandler from '@src/interactions/chat/session-handler';
+import Question from '@src/interactions/chat/types/question.type';
+
+jest.mock('@prisma/client', () => ({
+  PrismaClient: jest.fn().mockImplementation(() => ({
+    $connect: jest.fn(),
+    session: {
+      create: () => {},
+    },
+  })),
+  Prisma: {
+    validator: jest.fn(() => jest.fn()),
+  },
+  QuestionType: {
+    Single: 'Single',
+  },
+}));
 
 describe('SessionService', () => {
+  let spyPrismaSessionCreate: jest.SpyInstance;
+  let spyQuestionFindFirst: jest.SpyInstance;
+
+  let prismaSvc: PrismaService;
+  let questionSvc: QuestionService;
+
+  beforeAll(() => {
+    prismaSvc = new PrismaService();
+    questionSvc = new QuestionService();
+
+    spyPrismaSessionCreate = jest.spyOn(prismaSvc.session, 'create').mockReturnValue(Promise.resolve(({
+      chatId: '123',
+      createdAt: new Date('2021-01-01'),
+      currentQuestionId: 123,
+      currentQuestion: {
+        id: 123,
+        type: QuestionType.Single,
+        options: [],
+        actions: [],
+        parent: null,
+        parentId: null,
+        createdAt: new Date('2021-01-01'),
+        isActive: true,
+        title: 'test',
+      },
+      customer: 'customer',
+      id: 123,
+      lastInteraction: new Date('2021-01-01'),
+      status: 'status',
+    })) as any);
+
+    spyQuestionFindFirst = jest.spyOn(questionSvc, 'findFirst').mockReturnValue(Promise.resolve(new Question({
+      id: 123,
+      type: QuestionType.Single,
+      options: [],
+      actions: [],
+      parent: null,
+      parentId: null,
+      createdAt: new Date('2021-01-01'),
+      isActive: true,
+      title: 'test',
+    })));
+  });
+
   it('should be a singleton', () => {
     const instance1 = SessionService.getInstance();
     const instance2 = SessionService.getInstance();
     expect(instance1).toBe(instance2);
+  });
+
+  it('should create a session', async () => {
+    const sessionService = new SessionService(prismaSvc);
+    const session = await sessionService.create('123');
+
+    expect(session).toBeInstanceOf(SessionHandler);
+    expect(spyQuestionFindFirst).toBeCalledTimes(1);
+    expect(spyPrismaSessionCreate).toBeCalledWith(expect.objectContaining({
+      data: expect.objectContaining({
+        chatId: '123',
+      }),
+    }));
   });
 });
